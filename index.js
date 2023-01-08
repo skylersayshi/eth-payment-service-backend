32edb5cb49fb14a3ad69660fde808cfd8a3cb140
giantnerd123 <payneskyler@yahoo.com>
2023-01-07 20:09:43 -0800

    same

--- index.js
+++ index.js
@@ -13,9 +13,7 @@ dotenv.config();
 
 app.use(bodyParser.json({ limit: "30mb", extended: true }))
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }))
-app.use(cors({
-  origin: '*'
-}));
+app.use(cors());
 // app.use((req, res, next) => {
 //   res.append('Access-Control-Allow-Origin', 'https://rodeopay.xyz');
 //   res.append('Access-Control-Allow-Headers', 'Content-Type');
