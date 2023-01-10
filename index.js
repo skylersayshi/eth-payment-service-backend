2617158a344d3b7f1f2fbb9cb72d807df23521c5
giantnerd123 <payneskyler@yahoo.com>
2023-01-09 18:21:27 -0800

    again

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ dotenv.config();
 app.use(bodyParser.json({ limit: "30mb", extended: true }))
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }))
 const issue2options = {
-  origin: true,
+  origin: 'https://rodeopay.xyz',
   // methods: ["POST"],
   credentials: false,
 };
