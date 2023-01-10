50767adccbb2e2d9fe2b9a4dc9d2b54a1204e381
giantnerd123 <payneskyler@yahoo.com>
2023-01-09 17:09:02 -0800

    new try

--- index.js
+++ index.js
@@ -30,16 +30,34 @@ app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }))
 //   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
 //   next();
 // })
-var allowlist = ['https://rodeopay.xyz']
-var corsOptionsDelegate = function (req, callback) {
-  var corsOptions;
-  if (allowlist.indexOf(req.header('Origin')) !== -1) {
-    corsOptions = { origin: true } // reflect (enable) the requested origin in the CORS response
-  } else {
-    corsOptions = { origin: false } // disable CORS for this request
-  }
-  callback(null, corsOptions) // callback expects two parameters: error and options
-}
+// var allowlist = ['https://rodeopay.xyz']
+// var corsOptionsDelegate = function (req, callback) {
+//   var corsOptions;
+//   if (allowlist.indexOf(req.header('Origin')) !== -1) {
+//     corsOptions = { origin: true } // reflect (enable) the requested origin in the CORS response
+//   } else {
+//     corsOptions = { origin: false } // disable CORS for this request
+//   }
+//   callback(null, corsOptions) // callback expects two parameters: error and options
+// }
+app.use(function (req, res, next) {
+
+  // Website you wish to allow to connect
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8888');
+
+  // Request methods you wish to allow
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+  // Request headers you wish to allow
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  res.setHeader('Access-Control-Allow-Credentials', true);
+
+  // Pass to next layer of middleware
+  next();
+});
 app.use('/users', cors(corsOptionsDelegate), userRoutes)
 app.use('/requests', cors(corsOptionsDelegate), requestRoutes)
 app.use('/transactions', cors(corsOptionsDelegate), transactionRoutes)
