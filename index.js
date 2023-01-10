3c0b4793bc5dd7c7fa435580fc27104417813016
giantnerd123 <payneskyler@yahoo.com>
2023-01-09 20:48:23 -0800

    htaccess

--- index.js
+++ index.js
@@ -14,9 +14,7 @@ dotenv.config();
 app.use(bodyParser.json({ limit: "30mb", extended: true }))
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }))
 
-app.use(cors({
-  origin: 'https://rodeopay.xyz'
-}))
+app.use(cors())
 
 app.use('/users', userRoutes)
 app.use('/requests', requestRoutes)
