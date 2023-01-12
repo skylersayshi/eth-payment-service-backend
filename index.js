12eb54eebb59a8c5d4b7e7f3aa6e2e5842e8a2b5
giantnerd123 <payneskyler@yahoo.com>
2023-01-12 00:35:02 -0800

    test

--- index.js
+++ index.js
@@ -16,12 +16,12 @@ app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }))
 // app.use(express.json());
 // app.use(express.urlencoded({extended:true}));
 
-// app.use(cors())
-app.use(function(req,res,next){
-  res.header("Access-Control-Allow-Origin", "*");
-   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-   res.header('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS');
-})
+app.use(cors())
+// app.use(function(req,res,next){
+//   res.header("Access-Control-Allow-Origin", "*");
+//    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//    res.header('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS');
+// })
 
 // app.use(function (req, res, next) {
 //   res.setHeader('Access-Control-Allow-Origin', 'https://rodeopay.xyz');
