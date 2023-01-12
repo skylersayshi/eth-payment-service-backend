1567f634b17e790d843855b05d1b02cc9b39e257
giantnerd123 <payneskyler@yahoo.com>
2023-01-12 01:10:24 -0800

    test

--- index.js
+++ index.js
@@ -11,8 +11,14 @@ const app = express();
 
 dotenv.config();
 
-app.use(bodyParser.json({ limit: "30mb", extended: true }))
-app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }))
+app.use(bodyParser.json({ 
+  // limit: "30mb", 
+  extended: true 
+}))
+app.use(bodyParser.urlencoded({ 
+  // limit: "30mb", 
+  extended: true 
+}))
 
 app.use(cors())
 
